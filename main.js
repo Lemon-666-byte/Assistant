bcf656e41b0b2a093ab1986d5f3263ee79bc6670
yangjh <jianhong.yang@kikatech.com>
2023-01-12 21:16:36 +0800

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+"ui";
+
+$ui.layout(
+    <vertical>
+        <appbar>
+            <toolbar id="toolbar" title="小助手" />
+        </appbar>
+        <frame h="*">
+           
+        </frame>
+    </vertical>
+);
+
+activity.setSupportActionBar($ui.toolbar);
