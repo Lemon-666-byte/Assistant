ec8c1e7775df9f398c808819b7ebbdadad1d6123
yangjh <jianhong.yang@kikatech.com>
2023-01-17 11:50:35 +0800

    界面

--- openFish.js
+++ openFish.js
@@ -1,70 +1,41 @@
 auto.waitFor();
 limit();
 log("launch->");
-var fishPackageName = "com.taobao.idlefish";
-var launch = app.launchPackage(fishPackageName);
-// var obj=textContains('我的').findOne(4000);
-var obj=text("我的").findOne(5000)
+openMy();
+openMyPublish();
 
-log("obj->" + obj);
-// sleep(1000);
-// obj.parent().parent().click();
-// log("obj.parent()->" + obj.parent());
-// var x=obj.bounds().centerX()
-// var y=obj.bounds().centerY()
-// log("x->"+x);
-// log("y->"+y);
-// var fishPackageName = "com.taobao.idlefish";
-// var launch = app.launchPackage(fishPackageName);
-// if (launch) {
-//   log("launch->" + launch);
-//   sleep(1000);
-//   clickMy();
-// }
+function openMy() {
+  var fishPackageName = "com.taobao.idlefish";
+  var launch = app.launchPackage(fishPackageName);
+  if (launch == null) {
+    log("没有打开 咸鱼");
+    return;
+  }
+  sleep(1000);
+  var obj = text("我的").findOne(5000);
 
-// function clickMy() {
-//   log("我的");
-//   var tab = id("tab_title").text("我的").find();
-//   tab.setText("jjj")
-//   log(tab);
-//   // var my = className("android.widget.FrameLayout").filter(function (my) {
-//   //   return my.desc() == "我的,未选中状态";
-//   // });
-//   // if (my == null) {
-//   //   log("没有找到");
-//   // } else {
-//   //   log("找到我的");
-//   //   log(my);
-//   //   my.click();
-//   // }
-//   // var my = className("android.widget.FrameLayout")
-//   //   .desc("我的,未选中状态").depth(1).findOne();
-//   // if (my == null) {
-//   //   log("没有找到");
-//   // } else {
-//   //   log("找到我的");
-//   //   log(my);
-//   //   my.click();
-//   // }
-// }
+  if (obj != null) {
+    log("找到我的");
+    var x = obj.bounds().centerX();
+    var y = obj.bounds().centerY();
+    click(x, y);
+  } else {
+    log("没有找到我的");
+  }
+}
 
-// // className("TextView").text('我的').findOne().click()
-// // var my = text("我的").findOne(5000);
-// // if (my == null) {
-// //   log("没有找到");
-// // } else {
-// //   my.click();
-// // }
-// // }
+function openMyPublish() {
+  var myPublish = descStartsWith("我发布的").findOne(5000);
+  if (myPublish == null) {
+    log("没有找到我发布的");
+    return;
+  }
+  var x = myPublish.bounds().centerX();
+  var y = myPublish.bounds().centerY();
+  click(x, y);
+  log("我发布的->" + myPublish);
+}
 
-// // launchApp("BIYAPAY");
-// //   var my = id('menu_transaction').findOne(5000);
-// //   if (my == null) {
-// //     log("没有找到");
-// //   } else {
-// //     log("找到");
-// //     my.click();
-// //   }
 
 function limit() {
   importClass(
