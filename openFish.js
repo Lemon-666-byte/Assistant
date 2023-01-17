63f9a34209c1b2a920a9a17797a17d9a5bcb7aa3
yangjh <jianhong.yang@kikatech.com>
2023-01-18 01:46:24 +0800

    点击图片

--- openFish.js
+++ openFish.js
@@ -1,41 +1,98 @@
+var delayClickTime = 1000;
 auto.waitFor();
 limit();
 log("launch->");
-openMy();
-openMyPublish();
+openFish();
+sleep(delayClickTime);
+clickViewById("indicator_itmes");
+sleep(delayClickTime);
+clickViewByDesc("发闲置");
+sleep(delayClickTime);
+clickViewByDesc("描述");
+sleep(delayClickTime);
+var descText = "你好";
+inputText(descText);
+sleep(delayClickTime);
+clickViewByDesc("价格");
+sleep(delayClickTime);
+var price = "123";
+inputPrice(price);
+sleep(delayClickTime);
+clickViewByDesc("原价");
+sleep(delayClickTime);
+var originalPrice = "33333";
+inputPrice(originalPrice);
+clickViewByDesc("确定");
+sleep(delayClickTime);
+clickAddImage();
 
-function openMy() {
+function openFish() {
   var fishPackageName = "com.taobao.idlefish";
   var launch = app.launchPackage(fishPackageName);
   if (launch == null) {
     log("没有打开 咸鱼");
     return;
   }
-  sleep(1000);
-  var obj = text("我的").findOne(5000);
-
-  if (obj != null) {
-    log("找到我的");
-    var x = obj.bounds().centerX();
-    var y = obj.bounds().centerY();
-    click(x, y);
-  } else {
-    log("没有找到我的");
-  }
 }
 
-function openMyPublish() {
-  var myPublish = descStartsWith("我发布的").findOne(5000);
-  if (myPublish == null) {
-    log("没有找到我发布的");
+function clickViewById(viewId) {
+  var obj = id(viewId).findOne(5000);
+  if (obj == null) {
+    log("没有找到->" + viewId);
     return;
   }
-  var x = myPublish.bounds().centerX();
-  var y = myPublish.bounds().centerY();
+  var x = obj.bounds().centerX();
+  var y = obj.bounds().centerY();
   click(x, y);
-  log("我发布的->" + myPublish);
 }
 
+function clickViewByDesc(desc) {
+  var descObj = descStartsWith(desc).findOne(5000);
+  if (descObj == null) {
+    log("没有找到->" + descObj);
+    return;
+  }
+  var x = descObj.bounds().centerX();
+  var y = descObj.bounds().centerY();
+  press(x, y, 1);
+}
+
+function inputPrice(price) {
+  for (let i in price) {
+    var number = desc(price[i]).findOne(5000);
+    number.click();
+    sleep(600);
+  }
+}
+
+function inputText(str) {
+  auto.setWindowFilter(function (window) {
+    return true;
+  });
+  var jkl = str;
+  setClip("1");
+  sleep(500);
+  setClip(jkl);
+  sleep(600);
+  var gh = text(jkl).findOnce().click();
+  log(gh);
+}
+
+function clickAddImage() {
+  var descObj = descStartsWith("添加图片").findOne(5000);
+  if (descObj == null) {
+    log("没有找到->" + descObj);
+    return;
+  }
+  log("找到->" + descObj);
+  var bounds = descObj.bounds();
+  log("找到bounds->" + bounds.top);
+  var x = bounds.left + 100;
+  var y = bounds.top + 100;
+  log("找到x->" + x);
+  log("找到y->" + y);
+  click(x, y);
+}
 
 function limit() {
   importClass(
